827c60975e822db7736e224a7f9a2a202339bf86
muhammaddzakiardiansyah <muhammaddzakiardiansyah811@gmail.com>
2023-12-27 07:25:12 +0700

    add: page students

---
+++ components/presence/tablePrsence.php
@@ -0,0 +1,63 @@
+<div class="container-fluid py-4">
+    <div class="d-flex justify-content-between align-items-center">
+      <a href="presence.php?page=presence&even=add-presence" class="btn btn-primary">Add Presence</a>
+      <div class="input-group input-group-outline w-25">
+        <label class="form-label">Search</label>
+        <input type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12">
+      <div class="card my-4">
+        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+          <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+            <h6 class="text-white text-capitalize ps-3">Table Students</h6>
+          </div>
+        </div>
+        <div class="card-body px-0 pb-2">
+          <div class="table-responsive p-0">
+            <table class="table align-items-center mb-0">
+              <thead>
+                <tr>
+                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">NIS</th>
+                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Student Name</th>
+                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Class</th>
+                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Number Phone</th>
+                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Address</th>
+                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <div class="d-flex px-2 py-1">
+                      <h6 class="mb-0 text-sm">22,5698</h6>
+                    </div>
+                  </td>
+                  <td>
+                    <p class="text-xs font-weight-bold mb-0">M Dzaki Ardiansyah</p>
+                  </td>
+                  <td class="align-middle text-center text-sm">
+                    <p class="text-xs font-weight-bold mb-0">XI PPLG 1</p>
+                  </td>
+                  <td class="align-middle text-center">
+                    <p class="text-xs font-weight-bold mb-0">08946756352673</p>
+                  </td>
+                  <td>
+                    <p class="text-xs font-weight-bold mb-0">Jl KH Hasyim Asyari gg2 kel setono kec pekalongan timur kota pekalongan</p>
+                  </td>
+                  <td>
+                    <div class="ms-auto text-center">
+                      <a class="btn btn-link text-dark mb-0" href="javascript:;"><i class="material-icons text-sm me-2">edit</i>Edit</a>
+                      <a class="btn btn-link text-danger text-gradient mb-0" href="javascript:;"><i class="material-icons text-sm me-2">delete</i>Delete</a>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
